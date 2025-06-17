c9277fdad622e0ea4d48df146abf1c58e3d98bc2
Sena Altun <senaaltun@nokta.com>
2025-06-17 11:21:54 +0300

    Add version 6.5.5

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EmpowerMobileAds",
-            url: "https://cdn.empower.net/sdk/mobile/ios/EmpowerMobileAds-6.4.4.zip",
+            url: "https://cdn.empower.net/sdk/mobile/ios/EmpowerMobileAds-6.5.5.zip",
             checksum: "0c22bf3e232cd104df4bc2d20cb4c06b5e8c1008ad6adef3424e335fb72c6df3"
         )
     ]
