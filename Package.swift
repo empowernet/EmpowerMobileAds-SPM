263cb5db00115a48289ee428f113df6d6dfb6d0c
Sena Altun <senaaltun@nokta.com>
2025-06-18 11:42:02 +0300

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "EmpowerMobileAds",
-            targets: ["EmpowerMobileAds"]
+            targets: ["EmpowerMobileAdsWrapper"]
         )
     ],
     dependencies: [
@@ -23,37 +23,43 @@ let package = Package(
         )
     ],
     targets: [
+        // MARK: - Wrapper Target
+        .target(
+            name: "EmpowerMobileAdsWrapper",
+            dependencies: [
+                .target(name: "EmpowerMobileAds"),
+                .target(name: "AppLovinSDK"),
+                .target(name: "DTBiOSSDK"),
+                .target(name: "AppLovinMediationAmazonAdMarketplaceAdapter"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "GoogleInteractiveMediaAds", package: "swift-package-manager-google-interactive-media-ads-ios")
+            ],
+            path: "EmpowerMobileAdsTarget"
+        ),
+
+        // MARK: - Binary Targets
         .binaryTarget(
-            name: "EmpowerMobileAdsCore",
+            name: "EmpowerMobileAds",
             url: "https://cdn.empower.net/sdk/mobile/ios/EmpowerMobileAds-6.6.0.zip",
             checksum: "b58725bb93edf68d5a8e657a61cf5f86fec5e9c706fe821f64b8f50773c88b01"
         ),
+
         .binaryTarget(
             name: "AppLovinSDK",
             url: "https://cdn.empower.net/sdk/mobile/ios/AppLovinSDK.zip",
             checksum: "593fdef6e0bae41dfb805cbdc5d3323c1c1d216a50cfa0ea229f21ea4cded925"
         ),
+
         .binaryTarget(
             name: "DTBiOSSDK",
             url: "https://cdn.empower.net/sdk/mobile/ios/DTBiOSSDK.zip",
             checksum: "914bd6658513f69726fc93733ef65991202b4f134fb4e091110ec1054a5443aa"
         ),
+
         .binaryTarget(
             name: "AppLovinMediationAmazonAdMarketplaceAdapter",
             url: "https://cdn.empower.net/sdk/mobile/ios/AppLovinMediationAmazonAdMarketplaceAdapter.zip",
             checksum: "9616a54148fc39f2ed9ff373c92d81aa0164f77984ae6822dbd8bb21a6ee838b"
-        ),
-        .target(
-            name: "EmpowerMobileAds",
-            dependencies: [
-                "EmpowerMobileAdsCore",
-                "AppLovinSDK",
-                "DTBiOSSDK",
-                "AppLovinMediationAmazonAdMarketplaceAdapter",
-                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
-                .product(name: "GoogleInteractiveMediaAds", package: "swift-package-manager-google-interactive-media-ads-ios")
-            ],
-            path: "EmpowerMobileAdsTarget"
         )
     ]
 )
